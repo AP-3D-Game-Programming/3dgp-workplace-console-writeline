3f6cee15f015571a47577e4b4bce1c4097baf9e7
Alex Guo <alex.guo@student.ap.be>
2025-10-30 18:46:38 +0100

    reset bucket after click

--- FireLine/Assets/Scripts/Bucket.cs
+++ FireLine/Assets/Scripts/Bucket.cs
@@ -12,49 +12,61 @@ public class Bucket : MonoBehaviour
     private float rotation = 30f;
     private bool isRotatingBackward;
     private float roateSpeed = 3f;
+	private float tiltHoldTime = 0f; // Timer for how long to hold the tilt
+	private float tiltMaxHoldTime = 1f; // 3 seconds
 
-    public GameObject sphereColliderPrefab;
+	public GameObject sphereColliderPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isRotatingForward = false;
-        rotateThisFrame = 0;
-        isRotatingBackward = false;
-    }
+		isRotatingForward = false;
+		rotateThisFrame = 0;
+		isRotatingBackward = false;
+	}
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
 
-        if (Input.GetMouseButton(0))
-        {
-            if (rotateThisFrame == 0)
-            {
-                isRotatingForward = true;
-                transform.Find("water_splash").gameObject.SetActive(false);
-                transform.Find("water_splash").gameObject.SetActive(true);
-                Instantiate(sphereColliderPrefab, transform);
+		if (Input.GetMouseButton(0))
+		{
+			if (rotateThisFrame == 0)
+			{
+				isRotatingForward = true;
+				transform.Find("water_splash").gameObject.SetActive(false);
+				transform.Find("water_splash").gameObject.SetActive(true);
+				Instantiate(sphereColliderPrefab, transform);
+			}
+		}
 
-            }
-            else if (rotateThisFrame == rotation)
-                isRotatingBackward = true;
-        }
+		if (isRotatingForward)
+		{
+			transform.Rotate(Vector3.right * roateSpeed);
+			rotateThisFrame += roateSpeed;
+			if (rotateThisFrame >= rotation)
+			{
+				isRotatingForward = false;
+				tiltHoldTime = 0f; // Start timer
+			}
+		}
 
-        if (isRotatingForward)
-        {
-            transform.Rotate(Vector3.right * roateSpeed);
-            rotateThisFrame += roateSpeed;
-            if (rotateThisFrame >= rotation)
-                isRotatingForward = false;
-        }
+		// Wait 3 seconds, then tilt back
+		if (!isRotatingForward && !isRotatingBackward && rotateThisFrame >= rotation)
+		{
+			tiltHoldTime += Time.deltaTime;
+			if (tiltHoldTime >= tiltMaxHoldTime)
+			{
+				isRotatingBackward = true;
+			}
+		}
 
-        if (isRotatingBackward)
-        {
-            transform.Rotate(Vector3.left * roateSpeed);
-            rotateThisFrame -= roateSpeed;
-            if (rotateThisFrame <= 0)
-                isRotatingBackward = false;
-        }
-    }
+		if (isRotatingBackward)
+		{
+			transform.Rotate(Vector3.left * roateSpeed);
+			rotateThisFrame -= roateSpeed;
+			if (rotateThisFrame <= 0)
+				isRotatingBackward = false;
+		}
+	}
 }
